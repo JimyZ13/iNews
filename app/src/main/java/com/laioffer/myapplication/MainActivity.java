92c0351ce1cf2bafc6fdc3225888c5ee95cb19a9
Benran Zhang <jimyzhan@usc.edu>
2022-06-22 21:23:35 -0700

    user login page created, application structure altered

--- app/src/main/java/com/laioffer/myapplication/MainActivity.java
+++ app/src/main/java/com/laioffer/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.laioffer.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeTextView = findViewById(R.id.welcomeTextView);
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        Button submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                welcomeTextView.setText("Welcome " + name + ", your email is: " + email);
+            }
+        });
+
+
     }
 }
