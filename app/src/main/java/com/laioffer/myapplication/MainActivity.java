4f514453859778af439d9ac21988837a7b1a3881
Benran Zhang <jimyzhan@usc.edu>
2022-06-23 18:28:37 -0700

    setup pages as fragments on the bottom nav and binded view to clicking event

--- app/src/main/java/com/laioffer/myapplication/MainActivity.java
+++ app/src/main/java/com/laioffer/myapplication/MainActivity.java
@@ -4,20 +4,36 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.laioffer.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+               NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.nav_host_fragment);
+               navController = navHostFragment.getNavController();
+               NavigationUI.setupWithNavController(navView, navController);
+               NavigationUI.setupActionBarWithNavController(this, navController);
 
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+               return navController.navigateUp();
+    }
 }
+
 
 
